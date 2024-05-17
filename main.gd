c00e7c74a9ea6a9051b7d64d86a60b4fc3593d4e
Fernando <fernando.almmm@gmail.com>
2024-05-16 20:28:45 -0400

    Handling inputs

--- main.gd
+++ main.gd
@@ -4,3 +4,11 @@ func _ready():
 	print("Hello, world!")
 	$Label.text = "Hello, world!"
 	$Label.modulate = Color.GREEN
+
+func _input(event):
+	if event.is_action_pressed("my_Action"):
+		$Label.modulate = Color.RED
+		
+	if event.is_action_released("my_Action"):
+		$Label.modulate = Color.GREEN
+		
